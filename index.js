c8d4f314071c00118858fd9b041d4cb417377920
Arita Osmani <aritaaosmani@gmail.com>
2021-11-17 16:34:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let listOfItems=[["Milk",1.2],["Cocoa",2],["Salad",2],["Carrots",2],["Tomatoes",2.5],["Ready meals",5]];
+let item;
+let price;
+let sum = 0;
+while(item != 0){
+    item = prompt("Add an item to your shopping list. (Terminate by typing 0): ");
+    if(item == 0){
+        break;
+    }else{
+        price = Number(prompt("Enter the price of the item: "));
+        if(isNaN(price)){
+            alert("Wrong price input! Try again. ");
+        }
+        else{
+            listOfItems.push([item,price]);
+        }
+        
+    }
+    
+}
+
+for(let i = 0;i < listOfItems.length; i++){
+    console.log(listOfItems[i][0]+ " costs: "+listOfItems[i][1]+ "£");
+    sum += listOfItems[i][1];
+}
+console.log("Your items' cost in total is: "+sum+ "£");
